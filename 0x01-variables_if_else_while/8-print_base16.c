b2ea5817d6cf1f2517d089beb56649bd6ba4594c
Egberan <aiyidumoses@gmail.com>
2022-07-04 14:06:52 -0700

    f

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
+
 /**
-* main- prints all the numbers of base 16 in lowercase, followed by a new line.
+* main- prints hexadecimal string
 *
 * Return: Always (Success)
 */
@@ -11,11 +12,11 @@ for (c = '0' c < = '9'; c++
 {
 putchar(c)
 }
-for (c = 'a'; c < = ‘f'; c++
+for (c = 'a'; c <= ‘f'; c++
 {
 putchar(c)
 }
-putchar( '\n' );
+putchar('\n');
 return (0);
 }
 
