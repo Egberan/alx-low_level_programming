a1737b7fb600d3eaa9c40b014d8abde480cbbc3e
Egberan <aiyidumoses@gmail.com>
2022-07-25 04:39:10 -0700

    comitting task 1-3

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,15 +1,26 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 /**
-* main - prints the number of arguments passed into it.
-* @argc: argument count
-* @argv: arguments
+* _strdup - Returns a pointer to a newly-allocated space in memory
+*           containing a copy of the string given as parameter.
+* @str: The string to be copied.
 *
-* Return: 0
+* Return: If str == NULL or insufficient memory is available - NULL.
+*         Otherwise - a pointer to the duplicated string.
 */
-int main(int argc, char **argv)
+char *_strdup(char *str)
 {
-(void)argv;
-printf("%d\n", argc - 1);
-return (0);
+char *duplicate;
+int index, len = 0;
+if (str == NULL)
+return (NULL);
+for (index = 0; str[index]; index++)
+len++;
+duplicate = malloc(sizeof(char) * (len + 1));
+if (duplicate == NULL)
+return (NULL);
+for (index = 0; str[index]; index++)
+duplicate[index] = str[index];
+duplicate[len] = '\0';
+return (duplicate);
 }
