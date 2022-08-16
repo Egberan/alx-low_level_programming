3ef44b0f75a2186a297366b3b55f6b9345b5a92a
Egberan <aiyidumoses@gmail.com>
2022-08-16 15:35:28 -0700

    commiting lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,35 +1,21 @@
-#ifndef _LISTS_
-#define _LISTS_
-
-#include <stdio.h>
-#include <stdlib.h>
+#ifndef LIST_H
+#define LIST_H
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
 
 /**
  * struct listint_s - singly linked list
- * @n: integer
+ * @n: int value stored
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
 } listint_t;
-
-/**
- * struct listp_s - singly linked list
- * @p: pointers of nodes
- * @next: points to the next node
- *
- * Description: singly linked list of pointers
- */
-typedef struct listp_s
-{
-	void *p;
-	struct listp_s *next;
-} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -45,6 +31,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
+int is_visited(const listint_t *, const listint_t **, int);
 listint_t *find_listint_loop(listint_t *head);
 
 #endif
