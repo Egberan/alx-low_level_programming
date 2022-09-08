6befa19bb818490458001f98f3d60e018cc5497d
Egberan <106923849+Egberan@users.noreply.github.com>
2022-09-08 15:24:07 +0100

    Update 100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,46 +1,29 @@
 #include "main.h"
 
 /**
-* read_textfile - reads a text file and prints it to the standard output
-* @filename: name of the file to be read
-* @letters: number of letters to read and print
-*
-* Return: the number of letters printed, or 0 if it failed
-*/
+ * read_textfile - reads and prints content of a file
+ * @filename: file name
+ * @letters: number of letters to print
+ *
+ * Return: Number of chars printed or 0
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int s, t;
-	char *buf;
+	int fd, retval, tmp;
+	char *buff;
 
 	if (!filename)
 		return (0);
-
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+		return (0);
 	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	tmp = read(fd, buff, letters);
+	retval = write(1, buff, tmp);
+	free(buff);
+	if (fd == -1 || tmp == -1 || retval == -1 || retval != tmp)
 		return (0);
-
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-
-	s = read(fd, buf, letters);
-	if (s < 0)
-	{
-		free(buf);
-		return (0);
-	}
-	buf[s] = '\0';
-
 	close(fd);
-
-	t = write(STDOUT_FILENO, buf, s);
-	if (t < 0)
-	{
-		free(buf);
-		return (0);
-	}
-
-	free(buf);
-	return (t);
+	return (retval);
 }
+
